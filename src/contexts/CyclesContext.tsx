50aa6dce18e6442e371ad0d7907ec4663be61cb0
natalia <lnataliaperri@gmail.com>
2023-02-24 09:59:46 -0300

    aplying imer

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,6 +1,5 @@
 import { createContext, ReactNode, useState, useReducer } from 'react'
 import {
-  ActionTypes,
   addNewCycleAction,
   interruptCurrentCycleAction,
   markCurrentCycleAsFinishedAction,
